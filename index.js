78cc740cd89de05f2e9a42be8cb39e6ee4a09bfd
Yonnick <77168432+Yonnick-a@users.noreply.github.com>
2021-10-21 10:20:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ var image2 = document.querySelectorAll("img") [1];
 image2.setAttribute("src", imgSource2);
 
 if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "🚩 Alex Wins!";
+    document.querySelector("h1").innerHTML = "🚩 Player 1 wins!";
 }
 else if (randomNumber2 > randomNumber1) {
-    document.querySelector("h1").innerHTML = "🚩 Yonnick Wins! xD";
+    document.querySelector("h1").innerHTML = "🚩 Player 2 wins!";
 }
 else {
-    document.querySelector("h1").innerHTML = "It's a Draw! but I still win :P";
+    document.querySelector("h1").innerHTML = "It's a Draw!";
 }
